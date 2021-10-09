110d0640195f64090cc426a6cc46dc4efbc6a66e
Hyunjin CHA <gump.cha@gmail.com>
2021-10-09 13:58:12 +0900

    initial commit

---
+++ src/Corona.vue
@@ -0,0 +1,25 @@
+<template>
+  <CoronaData msg="코로나 확진자 데이터" />
+</template>
+
+
+<script>
+import CoronaDataVue from "./components/CoronaData.vue";
+export default {
+  name: "App",
+  components: {
+    CoronaData: CoronaDataVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CoronaData.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div>
+      <button @click="downloadExcel">엑셀로 다운받기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Xlsx from 'xlsx'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      excelData: [
+        {
+          날짜: '2021-10-01',
+          일별확진자: 1000,
+        },
+        {
+          날짜: '2021-10-02',
+          일별확진자: 2000,
+        }
+      ]
+    }
+  },
+  methods: {
+    downloadExcel () {
+      const workBook = Xlsx.utils.book_new()
+      const workSheet = Xlsx.utils.json_to_sheet(this.excelData)
+      Xlsx.utils.book_append_sheet(workBook, workSheet, 'sheet')
+      Xlsx.writeFile(workBook, '코로나-확진자수-일별.xlsx')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
